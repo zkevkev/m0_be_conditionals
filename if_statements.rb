12012cd1c3ccb5592973b21fe815f5373a086793
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-02-17 08:59:41 -0700

    Update if_statements.rb

--- if_statements.rb
+++ if_statements.rb
@@ -10,19 +10,19 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-  weather = 'snowy'
+weather = "sunny"
 
-  if weather == 'sunny'
-    p "sunscreen"
-  elsif weather == 'rainy'
-    p "umbrella"
-  elsif weather == 'snowy'
-    p "coat"
-  elsif weather == 'icy'
-    p "yak traks"
-  else
-    p "good to go!"
-  end
+if weather == "sunny"
+  p "sunscreen"
+elsif weather == "rainy"
+  p "umbrella"
+elsif weather == "snowy"
+  p "coat"
+elsif weather == "icy"
+  p "yak traks"
+else
+  p "good to go!"
+end
 
 # Experiment with manipulating the value held in variable 'weather'
 # to print something other than 'coat'
